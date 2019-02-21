604305b7ed5fad5efa18065be6cbca3f2692e7bb
Dustin Perry <dustin.perry@outlook.com>
2019-02-21 03:41:31 -0600

    Added type checks to validation.  Implemented Apply.

--- src/urlql/Tests/urlql.mergepatch.tests/MergePatchTests.cs
+++ src/urlql/Tests/urlql.mergepatch.tests/MergePatchTests.cs
@@ -7,6 +7,32 @@ using System.Text;
 
 namespace urlql.mergepatch.tests
 {
+    class MergePatchApply : Specification
+    {
+        protected JObject obj;
+        protected MergePatch<Foo> patch;
+        protected Foo testFoo;
+
+        protected override void Arrange()
+        {
+            testFoo = new Foo();
+            var patchFoo = new Foo() { Id = 42 };
+            obj = JObject.Parse(JsonConvert.SerializeObject(patchFoo));
+            patch = new MergePatch<Foo>(obj);
+        }
+
+        protected override void Act()
+        {
+            patch.Apply(testFoo);
+        }
+
+        [Test]
+        public void ChangedValue()
+        {
+            Assert.AreEqual(testFoo.Id, 42);
+        }
+    }
+
     class MergePatchWrongType : Specification
     {
         protected JObject obj;
@@ -21,7 +47,7 @@ namespace urlql.mergepatch.tests
 
         protected override void Act()
         {
-            isValid = patch.Validate();
+            isValid = patch.IsValid();
         }
 
         [Test]
@@ -45,13 +71,37 @@ namespace urlql.mergepatch.tests
 
         protected override void Act()
         {
-            isValid = patch.Validate();
+            isValid = patch.IsValid();
         }
 
         [Test]
         public void IsValid()
         {
             Assert.IsTrue(isValid);
+        }
+    }
+
+    class MergePatchWrongMemberType : Specification
+    {
+        protected JObject obj;
+        protected MergePatch<Foo> patch;
+        protected bool isValid;
+
+        protected override void Arrange()
+        {
+            obj = JObject.Parse(@"{""Id"":""asdf""}");
+            patch = new MergePatch<Foo>(obj);
+        }
+
+        protected override void Act()
+        {
+            isValid = patch.IsValid();
+        }
+
+        [Test]
+        public void IsNotValid()
+        {
+            Assert.IsFalse(isValid);
         }
     }
 
@@ -69,7 +119,7 @@ namespace urlql.mergepatch.tests
 
         protected override void Act()
         {
-            isValid = patch.Validate();
+            isValid = patch.IsValid();
         }
 
         [Test]
@@ -93,7 +143,7 @@ namespace urlql.mergepatch.tests
 
         protected override void Act()
         {
-            isValid = patch.Validate();
+            isValid = patch.IsValid();
         }
 
         [Test]
--- src/urlql/urlql.mergepatch/MergePatch.cs
+++ src/urlql/urlql.mergepatch/MergePatch.cs
@@ -19,18 +19,66 @@ namespace urlql.mergepatch
             Type = typeof(T);
         }
 
-        public bool Validate()
+        public bool IsValid()
         {
             List<string> memberNames = new List<string>();
 
-            memberNames.AddRange(Type.GetProperties().Where(p => p.SetMethod != null).Select(p => p.Name));
-            memberNames.AddRange(Type.GetFields().Where(f => f.IsPublic).Select(f => f.Name));
+            memberNames.AddRange(Type.GetProperties().Where(p => p.SetMethod != null).Select(p => p.Name.ToLowerInvariant()));
+            memberNames.AddRange(Type.GetFields().Where(f => f.IsPublic).Select(f => f.Name.ToLowerInvariant()));
 
-            return !this.Properties().Select(p => p.Name).Except(memberNames).Any();
+            if (this.Properties().Select(p => p.Name.ToLowerInvariant()).Except(memberNames).Any())
+            {
+                return false;
+            }
+
+            foreach (var prop in this.Properties())
+            {
+                try
+                {
+                    // TODO: Cleanup this uglyness and cache type info.
+                    // TODO: String handling is really permissive on input, we need to do hard checks for string literals and enforce that
+                    var x = Type.GetProperties().Where(p => p.SetMethod != null && p.Name.ToLowerInvariant() == prop.Name.ToLowerInvariant()).FirstOrDefault()?.PropertyType ?? Type.GetFields().Where(f => f.IsPublic && f.Name.ToLowerInvariant() == prop.Name.ToLowerInvariant()).FirstOrDefault()?.FieldType;
+                    System.Convert.ChangeType(prop.Value.ToString(), x);
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            // TODO: Handle value types structs, ignore objects and foreign keys (this can screw up relational Id fields/navigation properties
+            //       or start chucking lots of ugly exceptions at the data access layer for consumers)
+            // TODO: Consider member properties such as JsonIgnore, IgnoreDataMember, and NonSerialized
+
+            return true;
         }
 
         public void Apply(T target)
         {
+            if (target.GetType() != typeof(T))
+            {
+                return; // TODO: Throw good exception here
+            }
+
+            if (!IsValid())
+            {
+                return; // TODO: Throw good exception here
+            }
+
+            foreach (var prop in this.Properties())
+            {
+                var clrProperty = Type.GetProperties().Where(p => p.SetMethod != null && p.Name.ToLowerInvariant() == prop.Name.ToLowerInvariant()).FirstOrDefault();
+                if (clrProperty != null)
+                {
+                    clrProperty.SetValue(target, System.Convert.ChangeType(prop.Value.ToString(), clrProperty.PropertyType));
+                    continue;
+                }
+                var clrField = Type.GetFields().Where(f => f.IsPublic && f.Name.ToLowerInvariant() == prop.Name.ToLowerInvariant()).FirstOrDefault();
+                if (clrField != null)
+                {
+                    clrField.SetValue(target, System.Convert.ChangeType(prop.Value.ToString(), clrField.FieldType));
+                }
+            }
         }
     }
 }
