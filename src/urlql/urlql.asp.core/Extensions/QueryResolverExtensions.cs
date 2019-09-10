2893c720787468433e7972bf35303c9b7abda8c4
Dustin Perry <dustin.perry@outlook.com>
2019-09-10 18:30:01 -0500

    Added some helper IActionResult extension methods.

--- src/urlql/urlql.asp.core/Extensions/QueryResolverExtensions.cs
+++ src/urlql/urlql.asp.core/Extensions/QueryResolverExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,52 @@ namespace urlql.asp.core
         public async static Task<QueryResultViewModel> GetResultsAsViewModelAsync(this QueryResolver resolver)
         {
             return (await resolver.GetResultsAsync()).AsViewModel();
+        }
+
+        /// <summary>
+        /// Returns the result
+        /// </summary>
+        /// <param name="resolver"></param>
+        /// <returns>IActionResult of OkObjectResult containing the a QueryResultViewModel
+        /// or IActionResult of BadRequestObjectResult containing an error message
+        /// </returns>
+        public static IActionResult GetOkObjectResult(this QueryResolver resolver)
+        {
+            try
+            {
+                return new OkObjectResult(resolver.GetResults().AsViewModel());
+            }
+            catch (QueryException qEx)
+            {
+                return new BadRequestObjectResult(qEx.Message);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Returns the result
+        /// </summary>
+        /// <param name="resolver"></param>
+        /// <returns>IActionResult of OkObjectResult containing the a QueryResultViewModel
+        /// or IActionResult of BadRequestObjectResult containing an error message
+        /// </returns>
+        public async static Task<IActionResult> GetOkObjectResultAsync(this QueryResolver resolver)
+        {
+            try
+            {
+                return new OkObjectResult((await resolver.GetResultsAsync()).AsViewModel());
+            }
+            catch (QueryException qEx)
+            {
+                return new BadRequestObjectResult(qEx.Message);
+            }
+            catch
+            {
+                throw;
+            }
         }
     }
 }
