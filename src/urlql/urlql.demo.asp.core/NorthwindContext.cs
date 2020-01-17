56ebf498aceafbe5226352eb38c6ef39fcecff89
Dustin Perry <dustin.perry@outlook.com>
2020-01-17 12:26:45 -0600

    Added logging to demonstrate query translation.

--- src/urlql/urlql.demo.asp.core/NorthwindContext.cs
+++ src/urlql/urlql.demo.asp.core/NorthwindContext.cs
@@ -5,11 +5,16 @@ using System.Threading.Tasks;
 using System.IO;
 using Microsoft.EntityFrameworkCore;
 using urlql.demo.asp.core.Entities;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace urlql.demo.asp.core
 {
     public class NorthwindContext : DbContext
     {
+        public static readonly ILoggerFactory LoggerFactory
+            = new LoggerFactory(new[] { new ConsoleLoggerProvider((_, __) => true, true) });
+
         public DbSet<Customer> Customers { get; set; }
 
         public NorthwindContext()
@@ -18,6 +23,8 @@ namespace urlql.demo.asp.core
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            optionsBuilder.UseLoggerFactory(LoggerFactory)  //tie-up DbContext with LoggerFactory object
+                .EnableSensitiveDataLogging();
             optionsBuilder.UseSqlite("Filename=./northwind.sqlite");
         }
     }
